c9d26c30dd8ea2ea337096cddec0c5bd038a37e2
Lucas Nogueira <lucas_snogueira@hotmail.com>
2025-07-06 09:36:41 -0300

    remove comments

--- src/server.js
+++ src/server.js
@@ -1,30 +1,28 @@
-import http from 'node:http'
+import http from 'node:http';
 
-import { json } from './middlewares/json.js'
-import { routes } from './routes.js'
-// import { extractQueryParams } from './utils/extract-query-params.js'
+import { json } from './middlewares/json.js';
+import { routes } from './routes.js';
 
 const server = http.createServer(async (req, res) => {
-  const { method, url } = req
+  const { method, url } = req;
 
-  await json(req, res)
+  await json(req, res);
 
-  const route = routes.find(route => {
-    return route.method === method && route.path.test(url)
-  })
+  const route = routes.find((route) => {
+    return route.method === method && route.path.test(url);
+  });
 
   if (route) {
-    const routeParams = req.url.match(route.path)
+    const routeParams = req.url.match(route.path);
 
-    const { query, ...params } = routeParams.groups
+    const { query, ...params } = routeParams.groups;
 
-    req.params = params
-    req.query = query ? extractQueryParams(query) : {}
+    req.params = params;
 
-    return route.handler(req, res)
+    return route.handler(req, res);
   }
 
-  return res.writeHead(404).end()
-})
+  return res.writeHead(404).end();
+});
 
-server.listen(3333)
+server.listen(3333);
